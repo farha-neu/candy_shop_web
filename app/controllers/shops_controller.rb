48214c7ff1e1531d4f00ed769cb7f2eddb423b84
farha <farhajw@gmail.com>
2019-02-02 19:24:31 -0800

    render and creation of shops

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,8 +1,35 @@
 class ShopsController < ApplicationController
+
     layout 'home'
+
+    # GET /shops
+    # URL Helper: shops_path
     def index
-        
+    	# Find all shops
+        @shops = Shop.all
+        @shop_new = Shop.new
     end
 
+    # GET /shops/:id
+    # URL Helper: shops_path(:id)
+    def show
+       @shop = Shop.find(params[:id])
+    end
+
+    def create
+	    @shops = Shop.all
+      @shop_new = Shop.new(shop_params)
+      if @shop_new.save
+         redirect_to(shop_path(@shop_new))
+      else
+         render('index')
+      end
+    end
+
+     private
+
+		  def shop_params
+		    params.require(:shop).permit(:name)
+		  end
 
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,6 @@ class Shop < ApplicationRecord
     
     has_many :candies
     has_many :shelves
+
+   validates_presence_of :name
 end
